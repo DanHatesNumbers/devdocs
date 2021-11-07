7b32a1ad6e402fa48e066456fa57ba006c2852a4
Daniel Murphy <15730032+DanHatesNumbers@users.noreply.github.com>
2021-11-07 21:20:35 +0000

    Make use of HCL syntax highlighting in terraform docs

--- lib/docs/filters/terraform/clean_html.rb
+++ lib/docs/filters/terraform/clean_html.rb
@@ -11,12 +11,8 @@ module Docs
         end
 
         css('pre').each do |node|
-          if language = node['class'][/(json|shell|ruby)/, 1]
+          if language = node['class'][/(json|shell|ruby|hcl)/, 1]
             node['data-language'] = language
-          end
-          # HCL isn't currently supported by Prism, Ruby syntax does an acceptable job for now
-          if language = node['class'][/(hcl)/, 1]
-            node['data-language'] = 'ruby'
           end
           node.content = node.content
           node.remove_attribute('class')
