9ce8e23a2c6c5c5677fedbc1627e42e907a12dc8
Daniel Murphy <15730032+DanHatesNumbers@users.noreply.github.com>
2021-11-07 19:41:00 +0000

    Add prefixes to state backend names to clarify what page is documenting

--- lib/docs/filters/terraform/entries.rb
+++ lib/docs/filters/terraform/entries.rb
@@ -12,21 +12,33 @@ module Docs
       def get_name
         name ||= at_css('#inner h1').content
         name.delete! '»'
+
+        # Remove unneeded Function suffix from function pages because we group them under their own sub-type because there's a lot of them
         name.sub!(/Function\s+/, '')
-        name.strip!
+
+        # Handle state backends without descriptive names
+        if slug_components[:subsubcategory].eql?('backends') &&
+           !%w[index configuration].include?(slug_components[:page]) &&
+           !slug_components[:subcategory].eql?('state')
+          name.prepend 'State Backend - '
+        end
+
         name
       end
 
+      def slug_components
+        Hash[%i[category subcategory subsubcategory page].zip slug.split('/')]
+      end
+
       def get_type
-        category, subcategory, = *slug.split('/')
-        if category.eql?('cli')
+        if slug_components[:category].eql?('cli')
           if @@cli_overview_regexs.any? { |exp| exp.match? slug }
             'CLI Features'
           else
             'CLI Commands'
           end
-        elsif category.eql?('language')
-          if subcategory.eql? 'functions'
+        elsif slug_components[:category].eql?('language')
+          if slug_components[:subcategory].eql?('functions')
             'Language - Functions'
           else
             'Language'
